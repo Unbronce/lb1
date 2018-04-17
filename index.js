6dfe3014aa310362d163d3239f76b97598b4c8dc
Unbronce <38374515+Unbronce@users.noreply.github.com>
2018-04-17 23:40:05 +0300

    qq
    test

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+
+const formidable = require('formidable')
+
+const fs = require('fs');
+
+const app = express()
+
+const router = express.Router();
+
+app.listen(8080, () => console.log("server listen on port 8080"))
+
+app.use(express.static(__dirname));
+
+router.get("/", (req, res) => {
+  res.send("index")
+})
+
+router.get("/start", (req, res) => {
+  res.render("start.html")
+})
+
+router.get("/upload", (req, res) => {
+  res.render("upload.html")
+})
+
+router.post("/upload", (req,res) => {
+  const form = new formidable.IncomingForm();
+  res.send(form)
+})
+
+router.get("*", (req, res) => {
+  res.render("index.html")
+})
+
